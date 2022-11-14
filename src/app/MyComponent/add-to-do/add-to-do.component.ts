a78d6b41d7350f5ee981997e54b3acc25f9b07f3
abhilash-kaim <99829604+abhilash-kaim@users.noreply.github.com>
2022-11-14 18:26:01 +0530

    changes saved

---
+++ src/app/MyComponent/add-to-do/add-to-do.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-add-to-do',
+  templateUrl: './add-to-do.component.html',
+  styleUrls: ['./add-to-do.component.css']
+})
+export class AddToDoComponent implements OnInit {
+  title!: string;
+  desc!:string;
+  @Output() todoAdd: EventEmitter<Todo>=new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(){
+    const todo={
+      sno:8,
+      title:this.title,
+      desc:this.desc,
+      active:true
+    }
+    this.todoAdd.emit(todo);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { TodoItemComponent } from './myComponents/todo-item/todo-item.component';
+import { AddToDoComponent } from './MyComponent/add-to-do/add-to-do.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent,
+    AddToDoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
